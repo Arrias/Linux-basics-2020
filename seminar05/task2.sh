56cafd89b1b48b90b8084520e60620d57debb3c6
Albert Skalt <allber971@gmail.com>
2020-10-22 23:12:09 +0300

    Add hw5

--- seminar05/task2.sh
+++ seminar05/task2.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+left=$1
+right=$2
+git log --pretty=format:"%s" > log.txt
+
+touch "releases_notes.md"
+online=0
+
+while read -r LINE;
+   do
+    if [[ $LINE == "$left" ]]; then
+        online=1
+    fi
+    if [[ "$online" == "1" ]]; then
+        echo $LINE >> "release_notes.md"
+    fi
+    if [[ $LINE == "$right" ]]; then
+        break
+    fi
+   done < "log.txt"
+
--- seminar05/task3.sh
+++ seminar05/task3.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+path=$1
+commit=$(cat $path)
+if [[ "$commit" =~ "^issue-[0-9]{1,} *" ]];then
+  echo OK
+  exit 0
+else
+  echo fail
+  exit 1
+fi
